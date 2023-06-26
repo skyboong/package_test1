c203baf0d1900e8a17bde76d15c2f18a7dea83e6
BK Choi <stsboongkee@gmail.com>
2023-06-26 22:22:54 +0900

    5th

--- bktest/main.py
+++ bktest/main.py
@@ -1,8 +1,8 @@
 from util import util as f2
 
 def print_test(txt:str):
-    print("*** running in file1.py ")
+    print("*** running in main.py ")
     f2.print_test(txt)
 
 if __name__ == '__main__':
-    print_test("hello")
+    print_test("main hello")
